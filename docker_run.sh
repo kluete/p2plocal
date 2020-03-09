1ac3af88ab8f872073e6d33f3001dbc2f9c3f6ff
LSK <lsk@xps15tv>
2020-03-09 21:27:43 +0100

    can't kb-terminate docker

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,7 @@
 PWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run -it -v $DIR/../:/simplx -u $(id -u):$(id -g) --rm volatilebitfield/cpp:$i bash -c ./p2p.bin 
+echo $PWD
+echo $DIR
+
+docker run -it -v $DIR/:/p2p -u $(id -u):$(id -g) debian bash -c ./p2p/p2p.bin 
